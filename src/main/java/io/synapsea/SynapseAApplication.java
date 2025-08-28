7c36cf2cd12503074e1641018f7e56f728440c4d
lahiru <lahiru.d@getapplova.com>
2025-08-28 11:59:11 +0530

    Initial commit

---
+++ src/main/java/io/synapsea/SynapseAApplication.java
@@ -0,0 +1,13 @@
+package io.synapsea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynapseAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynapseAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/synapsea/SynapseAApplicationTests.java
@@ -0,0 +1,13 @@
+package io.synapsea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynapseAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
